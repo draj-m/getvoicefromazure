c6278cc9c80b0b30c3324b68628bc7428768f95a
draj-m <dheeraj@profjim.com>
2022-04-06 18:40:46 +0530

    method

--- main.py
+++ main.py
@@ -7,25 +7,30 @@ from fastapi.encoders import jsonable_encoder
 
 
 
+
 def get_speech(text, voice):
-    if voice == 'en-GB-LibbyNeural':
-        speech_config = speechsdk.SpeechConfig(subscription="b11d9472cc9d4f56a5522a6ef450b7f4", region="eastus")
-        speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3)
-        synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
-        processed_text = text
-        audioMime = 'audio/mp3'
-        result = synthesizer.speak_text_async(text).get()
-        stream = speechsdk.AudioDataStream(result)
-        # stream.save_to_wav_file("bg1.mp3")
-        raw_bytes = []
-        x = 1
-        y = bytes
-        while x != 0:
-            audio_buffer = bytes(1000000)
-            x = stream.read_data(audio_buffer=audio_buffer)
-            raw_bytes.append(audio_buffer)
-            #y = y + audio_buffer
-        return text, processed_text, audioMime, raw_bytes
+    output = {}
+    speech_config = speechsdk.SpeechConfig(subscription="b11d9472cc9d4f56a5522a6ef450b7f4", region="eastus")
+    speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3)
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+    processed_text = text
+    audioMime = 'audio/mp3'
+    result = synthesizer.speak_text_async(text).get()
+    stream = speechsdk.AudioDataStream(result)
+    # stream.save_to_wav_file("bg1.mp3")
+    raw_bytes = []
+    x = 1
+    y = bytes
+    while x != 0:
+        audio_buffer = bytes(1000000)
+        x = stream.read_data(audio_buffer=audio_buffer)
+        raw_bytes.append(audio_buffer)
+    #y = y + audio_buffer
+    output['text'] = text
+    output['processed_text'] = processed_text
+    output['audioMime'] = audioMime
+    output['raw_bytes'] = raw_bytes
+    return output
 
 
 app = FastAPI()
@@ -36,11 +41,11 @@ class Item(BaseModel):
     voice: str
 
 
-@app.post("/v1/voice")
+@app.post("/v1")
 async def voices(item: Item):
     text = item.text
     voice = item.voice
     Output = get_speech(text, voice)
-    json_compatible_item_data = jsonable_encoder(Output, custom_encoder={
+    Output['raw_bytes'] = jsonable_encoder(Output['raw_bytes'], custom_encoder={
         bytes: lambda v: base64.b64encode(v).decode('utf-8')})
-    return json_compatible_item_data
+    return Output
